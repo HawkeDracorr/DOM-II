448cc010b2be5b49a8b9f69baee5c68111df5529
HawkeDracorr <lilith.hawkedracorr@gmail.com>
2019-06-18 17:13:18 -0500

    Added 4 events.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
 // Your code goes here
 // Wolves of a feather, awoo toghether
+
+let nav = document.querySelectorAll('.nav-link');
+
+nav[0].addEventListener('click', clickyNav);
+nav[1].addEventListener('click', clickyNav2);
+nav[2].addEventListener('click', clickyNav3);
+nav[3].addEventListener('click', clickyNav4);
+
+
+function clickyNav() {
+  // obj.style.color = 'red'
+  alert ('This Link goes nowhere, kid.')
+};
+function clickyNav2() {
+  // obj.style.color = 'red'
+  alert ('Still goes nowhere...')
+};
+function clickyNav3() {
+  // obj.style.color = 'red'
+  alert ('You thought wrong, dude.')
+};
+function clickyNav4() {
+  // obj.style.color = 'red'
+  alert ('Denied.')
+};
