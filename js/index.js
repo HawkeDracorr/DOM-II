a456af550c772ccb68210b9398990f7c5f92bf14
HawkeDracorr <lilith.hawkedracorr@gmail.com>
2019-06-18 17:29:38 -0500

    Added Mouseover and Mouseout events

--- js/index.js
+++ js/index.js
@@ -1,27 +1,39 @@
 // Your code goes here
 // Wolves of a feather, awoo toghether
 
-let nav = document.querySelectorAll('.nav-link');
 
+// Event 1: click
+let nav = document.querySelectorAll('.nav-link');
 nav[0].addEventListener('click', clickyNav);
 nav[1].addEventListener('click', clickyNav2);
 nav[2].addEventListener('click', clickyNav3);
 nav[3].addEventListener('click', clickyNav4);
 
-
 function clickyNav() {
-  // obj.style.color = 'red'
   alert ('This Link goes nowhere, kid.')
 };
 function clickyNav2() {
-  // obj.style.color = 'red'
   alert ('Still goes nowhere...')
 };
 function clickyNav3() {
-  // obj.style.color = 'red'
   alert ('You thought wrong, dude.')
 };
 function clickyNav4() {
-  // obj.style.color = 'red'
   alert ('Denied.')
 };
+
+// Event 2: Mouseover & Event 3: Mouseout
+let bodyContent = document.querySelectorAll('.content-section');
+bodyContent[0].addEventListener('mouseover', highlight)
+bodyContent[0].addEventListener('mouseout', revert)
+
+bodyContent[1].addEventListener('mouseover', highlight)
+bodyContent[1].addEventListener('mouseout', revert)
+
+function highlight() {
+  this.style.background = "red"
+  // alert ('test')
+}
+function revert() {
+  this.style.background = "transparent"
+}
